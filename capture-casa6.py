bd228ad3675c38bcc4f3d33573713530b5bac2b5
ruta-k <38125415+ruta-k@users.noreply.github.com>
2021-12-07 10:59:55 +0530

    Diagnostic plots

--- capture-casa6.py
+++ capture-casa6.py
@@ -371,84 +371,92 @@ if flaginit == True:
 #####################################################################
 # Calibration begins.
 if doinitcal == True:
-        assert os.path.isdir(msfilename)
-        try:
-                assert os.path.isdir(msfilename), "doinitcal = True but ms file not found."
-        except AssertionError:
-                logging.info("doinitcal = True but ms file not found.")
-                sys.exit()
-        mycalsuffix = ''
-        casalog.filter('INFO')
-        clearcal(vis=msfilename)
-        for i in range(0,len(myampcals)):
-                default(setjy)
-                setjy(vis=msfilename, spw=flagspw, field=myampcals[i])
+	assert os.path.isdir(msfilename)
+	try:
+		assert os.path.isdir(msfilename), "doinitcal = True but ms file not found."
+	except AssertionError:
+		logging.info("doinitcal = True but ms file not found.")
+		sys.exit()
+	mycalsuffix = ''
+	casalog.filter('INFO')
+	clearcal(vis=msfilename)
+	for i in range(0,len(myampcals)):
+		default(setjy)
+		setjy(vis=msfilename, spw=flagspw, field=myampcals[i])
 # Delay calibration  using the first flux calibrator in the list - should depend on which is less flagged
-        if os.path.isdir(str(msfilename)+'.K1'+mycalsuffix) == True:
-                os.system('rm -rf '+str(msfilename)+'.K1'+mycalsuffix)
-        gaincal(vis=msfilename, caltable=str(msfilename)+'.K1'+mycalsuffix, spw =flagspw, field=myampcals[0], 
-                solint='60s', refant=ref_ant,        solnorm= True, gaintype='K', gaintable=[], parang=True)
-        kcorrfield =myampcals[0]
+	gntable=str(msfilename)+'.K1'+mycalsuffix
+	#if os.path.isdir(str(msfilename)+'.K1'+mycalsuffix) == True:
+	#	os.system('rm -rf '+str(msfilename)+'.K1'+mycalsuffix)
+	if os.path.isdir(gntable) == True:
+		os.system('rm -rf '+gntable)
+	gaincal(vis=msfilename, caltable=gntable, spw =flagspw, field=myampcals[0], 
+		solint='60s', refant=ref_ant, solnorm= True, gaintype='K', gaintable=[], parang=True)
+	kcorrfield =myampcals[0]
 # an initial bandpass
-        if os.path.isdir(str(msfilename)+'.AP.G0'+mycalsuffix) == True:
-                os.system('rm -rf '+str(msfilename)+'.AP.G0'+mycalsuffix)
-        default(gaincal)
-        gaincal(vis=msfilename, caltable=str(msfilename)+'.AP.G0'+mycalsuffix, append=True, field=str(','.join(mybpcals)), 
-                spw =flagspw, solint = 'int', refant = ref_ant, minsnr = 2.0, solmode = 'L1R', gaintype = 'G', calmode = 'ap', gaintable = [str(msfilename)+'.K1'+mycalsuffix],
-                interp = ['nearest,nearestflag', 'nearest,nearestflag' ], parang = True)
-        if os.path.isdir(str(msfilename)+'.B1'+mycalsuffix) == True:
-                os.system('rm -rf '+str(msfilename)+'.B1'+mycalsuffix)
-        default(bandpass)
-        bandpass(vis=msfilename, caltable=str(msfilename)+'.B1'+mycalsuffix, spw =flagspw, field=str(','.join(mybpcals)), solint='inf', refant=ref_ant, solnorm = True,
-                minsnr=2.0, fillgaps=8, parang = True, gaintable=[str(msfilename)+'.K1'+mycalsuffix,str(msfilename)+'.AP.G0'+mycalsuffix], interp=['nearest,nearestflag','nearest,nearestflag'])
+	gntable=str(msfilename)+'.AP.G0'+mycalsuffix
+	#if os.path.isdir(str(msfilename)+'.AP.G0'+mycalsuffix) == True:
+	#	os.system('rm -rf '+str(msfilename)+'.AP.G0'+mycalsuffix)
+	if os.path.isdir(gntable) == True:
+		os.system('rm -rf '+gntable)
+	default(gaincal)
+	gaincal(vis=msfilename, caltable=gntable, append=True, field=str(','.join(mybpcals)), 
+		spw =flagspw, solint = 'int', refant = ref_ant, minsnr = 2.0, solmode = 'L1R', gaintype = 'G', calmode = 'ap', 
+		gaintable = [str(msfilename)+'.K1'+mycalsuffix], interp = ['nearest,nearestflag', 'nearest,nearestflag' ], parang = True)
+	if os.path.isdir(str(msfilename)+'.B1'+mycalsuffix) == True:
+		os.system('rm -rf '+str(msfilename)+'.B1'+mycalsuffix)
+	bptable=str(msfilename)+'.B1'+mycalsuffix
+	default(bandpass)
+	bandpass(vis=msfilename, caltable=bptable, spw =flagspw, field=str(','.join(mybpcals)), solint='inf', refant=ref_ant, solnorm = True,
+		minsnr=2.0, fillgaps=8, parang = True, gaintable=[str(msfilename)+'.K1'+mycalsuffix,str(msfilename)+'.AP.G0'+mycalsuffix], interp=['nearest,nearestflag','nearest,nearestflag'])
 # do a gaincal on all calibrators
-        mycals=myampcals+mypcals
-        i=0
-        if os.path.isdir(str(msfilename)+'.AP.G'+mycalsuffix) == True:
-                os.system('rm -rf '+str(msfilename)+'.AP.G'+mycalsuffix)
-        for i in range(0,len(mycals)):
-                mygaincal_ap2(msfilename,mycals[i],ref_ant,gainspw,uvracal,mycalsuffix)
+	mycals=myampcals+mypcals
+	i=0
+	if os.path.isdir(str(msfilename)+'.AP.G'+mycalsuffix) == True:
+		os.system('rm -rf '+str(msfilename)+'.AP.G'+mycalsuffix)
+	for i in range(0,len(mycals)):
+		mygaincal_ap2(msfilename,mycals[i],ref_ant,gainspw,uvracal,mycalsuffix)
 # Get flux scale
-        if os.path.isdir(str(msfilename)+'.fluxscale'+mycalsuffix) == True:
-                os.system('rm -rf '+str(msfilename)+'.fluxscale'+mycalsuffix)
+	if os.path.isdir(str(msfilename)+'.fluxscale'+mycalsuffix) == True:
+		os.system('rm -rf '+str(msfilename)+'.fluxscale'+mycalsuffix)
 ######################################
-        if mypcals !=[]:
-                if '3C286' in myampcals:
-                        myfluxscale= getfluxcal2(msfilename,'3C286',str(', '.join(mypcals)),mycalsuffix)
-                        myfluxscaleref = '3C286'
-                elif '3C147' in myampcals:
-                        myfluxscale= getfluxcal2(msfilename,'3C147',str(', '.join(mypcals)),mycalsuffix)
-                        myfluxscaleref = '3C147'
-                else:
-                        myfluxscale= getfluxcal2(msfilename,myampcals[0],str(', '.join(mypcals)),mycalsuffix)
-                        myfluxscaleref = myampcals[0]
-                logging.info(myfluxscale)
-                mygaintables =[str(msfilename)+'.fluxscale'+mycalsuffix,str(msfilename)+'.K1'+mycalsuffix, str(msfilename)+'.B1'+mycalsuffix]
-        else:
-                mygaintables =[str(msfilename)+'.AP.G'+mycalsuffix,str(msfilename)+'.K1'+mycalsuffix, str(msfilename)+'.B1'+mycalsuffix]
+	if mypcals !=[]:
+		if '3C286' in myampcals:
+			myfluxscale= getfluxcal2(msfilename,'3C286',str(', '.join(mypcals)),mycalsuffix)
+			myfluxscaleref = '3C286'
+		elif '3C147' in myampcals:
+			myfluxscale= getfluxcal2(msfilename,'3C147',str(', '.join(mypcals)),mycalsuffix)
+			myfluxscaleref = '3C147'
+		else:
+			myfluxscale= getfluxcal2(msfilename,myampcals[0],str(', '.join(mypcals)),mycalsuffix)
+			myfluxscaleref = myampcals[0]
+		logging.info(myfluxscale)
+		mygaintables =[str(msfilename)+'.fluxscale'+mycalsuffix,str(msfilename)+'.K1'+mycalsuffix, str(msfilename)+'.B1'+mycalsuffix]
+	else:
+		mygaintables =[str(msfilename)+'.AP.G'+mycalsuffix,str(msfilename)+'.K1'+mycalsuffix, str(msfilename)+'.B1'+mycalsuffix]
 ##############################
-        for i in range(0,len(myampcals)):
-                default(applycal)
-                applycal(vis=msfilename, field=myampcals[i], spw = flagspw, gaintable=mygaintables, gainfield=[myampcals[i],'',''], 
-                         interp=['nearest','',''], calwt=[False], parang=False)
+	for i in range(0,len(myampcals)):
+		default(applycal)
+		applycal(vis=msfilename, field=myampcals[i], spw = flagspw, gaintable=mygaintables, gainfield=[myampcals[i],'',''], 
+			interp=['nearest','',''], calwt=[False], parang=False)
 #For phase calibrator:
-        if mypcals !=[]:
-                default(applycal)
-                applycal(vis=msfilename, field=str(', '.join(mypcals)), spw = flagspw, gaintable=mygaintables, gainfield=str(', '.join(mypcals)), 
-                         interp=['nearest','','nearest'], calwt=[False], parang=False)
+	if mypcals !=[]:
+		default(applycal)
+		applycal(vis=msfilename, field=str(', '.join(mypcals)), spw = flagspw, gaintable=mygaintables, gainfield=str(', '.join(mypcals)), 
+			interp=['nearest','','nearest'], calwt=[False], parang=False)
 #For the target:
-        if target ==True:
-                if mypcals !=[]:
-                        default(applycal)
-                        applycal(vis=msfilename, field=str(', '.join(mytargets)), spw = flagspw, gaintable=mygaintables,
-                                 gainfield=[str(', '.join(mypcals)),'',''],interp=['linear','','nearest'], calwt=[False], parang=False)
-                else:
-                        default(applycal)
-                        applycal(vis=msfilename, field=str(', '.join(mytargets)), spw = flagspw, gaintable=mygaintables,
-                                 gainfield=[str(', '.join(myampcals)),'',''],interp=['linear','','nearest'], calwt=[False], parang=False)        
-        logging.info("Finished initial calibration.")
-        logging.info("A flagging summary is provided for the MS file.")
-        flagsummary(msfilename)
+	if target ==True:
+		if mypcals !=[]:
+			default(applycal)
+			applycal(vis=msfilename, field=str(', '.join(mytargets)), spw = flagspw, gaintable=mygaintables,
+				gainfield=[str(', '.join(mypcals)),'',''],interp=['linear','','nearest'], calwt=[False], parang=False)
+		else:
+			default(applycal)
+			applycal(vis=msfilename, field=str(', '.join(mytargets)), spw = flagspw, gaintable=mygaintables,
+				gainfield=[str(', '.join(myampcals)),'',''],interp=['linear','','nearest'], calwt=[False], parang=False)        
+	plot_files = my_dig_plot(msfilename,myfields,myampcals,mypcals,mytargets,mycalsuffix,bptable,gntable)
+	logging.info("Finished initial calibration.")
+	logging.info("A flagging summary is provided for the MS file.")
+	flagsummary(msfilename)
 #############################################################################3
 #######Ishwar post calibration flagging
 if doflag == True:
@@ -517,86 +525,94 @@ if doflag == True:
 #################### new redocal #########################3
 # Calibration begins.
 if redocal == True:
-        try:
-                assert os.path.isdir(msfilename), "redocal = True but ms file not found."
-        except AssertionError:
-                logging.info("redocal = True but ms file not found.")
-                sys.exit()
-        logging.info("You have chosen to redo the calibration on your data.")
-        mycalsuffix = 'recal'
-        casalog.filter('INFO')
-        clearcal(vis=msfilename)
-        for i in range(0,len(myampcals)):
-                default(setjy)
-                setjy(vis=msfilename, spw=flagspw, field=myampcals[i])
-                logging.info("Done setjy on %s"%(myampcals[i]))
+	try:
+		assert os.path.isdir(msfilename), "redocal = True but ms file not found."
+	except AssertionError:
+		logging.info("redocal = True but ms file not found.")
+		sys.exit()
+	logging.info("You have chosen to redo the calibration on your data.")
+	mycalsuffix = 'recal'
+	casalog.filter('INFO')
+	clearcal(vis=msfilename)
+	for i in range(0,len(myampcals)):
+		default(setjy)
+		setjy(vis=msfilename, spw=flagspw, field=myampcals[i])
+		logging.info("Done setjy on %s"%(myampcals[i]))
 # Delay calibration  using the first flux calibrator in the list - should depend on which is less flagged
-        if os.path.isdir(str(msfilename)+'.K1'+mycalsuffix) == True:
-                os.system('rm -rf '+str(msfilename)+'.K1'+mycalsuffix)
-        gaincal(vis=msfilename, caltable=str(msfilename)+'.K1'+mycalsuffix, spw =flagspw, field=myampcals[0], 
-                solint='60s', refant=ref_ant,        solnorm= True, gaintype='K', gaintable=[], parang=True)
-        kcorrfield =myampcals[0]
+	gntable=str(msfilename)+'.K1'+mycalsuffix
+	#if os.path.isdir(str(msfilename)+'.K1'+mycalsuffix) == True:
+	#	os.system('rm -rf '+str(msfilename)+'.K1'+mycalsuffix)
+	if os.path.isdir(gntable) == True:
+		os.system('rm -rf '+gntable)
+	gaincal(vis=msfilename, caltable=gntable, spw =flagspw, field=myampcals[0], 
+		solint='60s', refant=ref_ant,        solnorm= True, gaintype='K', gaintable=[], parang=True)
+	kcorrfield =myampcals[0]
 #        print 'wrote table',str(msfilename)+'.K1'+mycalsuffix
 # an initial bandpass
-        if os.path.isdir(str(msfilename)+'.AP.G0'+mycalsuffix) == True:
-                os.system('rm -rf '+str(msfilename)+'.AP.G0'+mycalsuffix)
-        default(gaincal)
-        gaincal(vis=msfilename, caltable=str(msfilename)+'.AP.G0'+mycalsuffix, append=True, field=str(','.join(mybpcals)), 
-                spw =flagspw, solint = 'int', refant = ref_ant, minsnr = 2.0, solmode ='L1R', gaintype = 'G', calmode = 'ap', gaintable = [str(msfilename)+'.K1'],
-                interp = ['nearest,nearestflag', 'nearest,nearestflag' ], parang = True)
-        if os.path.isdir(str(msfilename)+'.B1'+mycalsuffix) == True:
-                os.system('rm -rf '+str(msfilename)+'.B1'+mycalsuffix)
-        default(bandpass)
-        bandpass(vis=msfilename, caltable=str(msfilename)+'.B1'+mycalsuffix, spw =flagspw, field=str(','.join(mybpcals)), solint='inf', refant=ref_ant, solnorm = True,
-                minsnr=2.0, fillgaps=8, parang = True, gaintable=[str(msfilename)+'.K1'+mycalsuffix,str(msfilename)+'.AP.G0'+mycalsuffix], interp=['nearest,nearestflag','nearest,nearestflag'])
+	gntable=str(msfilename)+'.AP.G0'+mycalsuffix
+	#if os.path.isdir(str(msfilename)+'.AP.G0'+mycalsuffix) == True:
+	#	os.system('rm -rf '+str(msfilename)+'.AP.G0'+mycalsuffix)
+	if os.path.isdir(gntable) == True:
+		os.system('rm -rf '+gntable)
+	default(gaincal)
+	gaincal(vis=msfilename, caltable=gntable, append=True, field=str(','.join(mybpcals)), 
+		spw =flagspw, solint = 'int', refant = ref_ant, minsnr = 2.0, solmode ='L1R', gaintype = 'G', calmode = 'ap', gaintable = [str(msfilename)+'.K1'],
+		interp = ['nearest,nearestflag', 'nearest,nearestflag' ], parang = True)
+	if os.path.isdir(str(msfilename)+'.B1'+mycalsuffix) == True:
+		os.system('rm -rf '+str(msfilename)+'.B1'+mycalsuffix)
+	bptable=str(msfilename)+'.B1'+mycalsuffix
+	default(bandpass)
+	bandpass(vis=msfilename, caltable=bptable, spw =flagspw, field=str(','.join(mybpcals)), solint='inf', refant=ref_ant, solnorm = True,
+		minsnr=2.0, fillgaps=8, parang = True, gaintable=[str(msfilename)+'.K1'+mycalsuffix,str(msfilename)+'.AP.G0'+mycalsuffix], interp=['nearest,nearestflag','nearest,nearestflag'])
 # do a gaingal on all calibrators
-        mycals=myampcals+mypcals
-        i=0
-        if os.path.isdir(str(msfilename)+'.AP.G'+mycalsuffix) == True:
-                os.system('rm -rf '+str(msfilename)+'.AP.G'+mycalsuffix)
-        for i in range(0,len(mycals)):
-                mygaincal_ap2(msfilename,mycals[i],ref_ant,gainspw,uvracal,mycalsuffix)
+	mycals=myampcals+mypcals
+	i=0
+	if os.path.isdir(str(msfilename)+'.AP.G'+mycalsuffix) == True:
+		os.system('rm -rf '+str(msfilename)+'.AP.G'+mycalsuffix)
+	for i in range(0,len(mycals)):
+		mygaincal_ap2(msfilename,mycals[i],ref_ant,gainspw,uvracal,mycalsuffix)
 # Get flux scale
-        if os.path.isdir(str(msfilename)+'.fluxscale'+mycalsuffix) == True:
-                os.system('rm -rf '+str(msfilename)+'.fluxscale'+mycalsuffix)
+	if os.path.isdir(str(msfilename)+'.fluxscale'+mycalsuffix) == True:
+		os.system('rm -rf '+str(msfilename)+'.fluxscale'+mycalsuffix)
 ########################################
-        if mypcals !=[]:
-                if '3C286' in myampcals:
-                        myfluxscale= getfluxcal2(msfilename,'3C286',str(', '.join(mypcals)),mycalsuffix)
-                        myfluxscaleref = '3C286'
-                elif '3C147' in myampcals:
-                        myfluxscale= getfluxcal2(msfilename,'3C147',str(', '.join(mypcals)),mycalsuffix)
-                        myfluxscaleref = '3C147'
-                else:
-                        myfluxscale= getfluxcal2(msfilename,myampcals[0],str(', '.join(mypcals)),mycalsuffix)
-                        myfluxscaleref = myampcals[0]
-                logging.info(myfluxscale)
-                mygaintables =[str(msfilename)+'.fluxscale'+mycalsuffix,str(msfilename)+'.K1'+mycalsuffix, str(msfilename)+'.B1'+mycalsuffix]
-        else:
-                mygaintables =[str(msfilename)+'.AP.G'+mycalsuffix,str(msfilename)+'.K1'+mycalsuffix, str(msfilename)+'.B1'+mycalsuffix]
+	if mypcals !=[]:
+		if '3C286' in myampcals:
+			myfluxscale= getfluxcal2(msfilename,'3C286',str(', '.join(mypcals)),mycalsuffix)
+			myfluxscaleref = '3C286'
+		elif '3C147' in myampcals:
+			myfluxscale= getfluxcal2(msfilename,'3C147',str(', '.join(mypcals)),mycalsuffix)
+			myfluxscaleref = '3C147'
+		else:
+			myfluxscale= getfluxcal2(msfilename,myampcals[0],str(', '.join(mypcals)),mycalsuffix)
+			myfluxscaleref = myampcals[0]
+		logging.info(myfluxscale)
+		mygaintables =[str(msfilename)+'.fluxscale'+mycalsuffix,str(msfilename)+'.K1'+mycalsuffix, str(msfilename)+'.B1'+mycalsuffix]
+	else:
+		mygaintables =[str(msfilename)+'.AP.G'+mycalsuffix,str(msfilename)+'.K1'+mycalsuffix, str(msfilename)+'.B1'+mycalsuffix]
 ###############################################################
-        for i in range(0,len(myampcals)):
-                default(applycal)
-                applycal(vis=msfilename, field=myampcals[i], spw = flagspw, gaintable=mygaintables, gainfield=[myampcals[i],'',''], 
-                         interp=['nearest','',''], calwt=[False], parang=False)
+	for i in range(0,len(myampcals)):
+		default(applycal)
+		applycal(vis=msfilename, field=myampcals[i], spw = flagspw, gaintable=mygaintables, gainfield=[myampcals[i],'',''], 
+			interp=['nearest','',''], calwt=[False], parang=False)
 #For phase calibrator:
-        if mypcals !=[]:
-                default(applycal)
-                applycal(vis=msfilename, field=str(', '.join(mypcals)), spw = flagspw, gaintable=mygaintables, gainfield=str(', '.join(mypcals)), 
-                         interp=['nearest','','nearest'], calwt=[False], parang=False)
+	if mypcals !=[]:
+		default(applycal)
+		applycal(vis=msfilename, field=str(', '.join(mypcals)), spw = flagspw, gaintable=mygaintables, gainfield=str(', '.join(mypcals)), 
+			interp=['nearest','','nearest'], calwt=[False], parang=False)
 #For the target:
-        if target ==True:
-                if mypcals !=[]:
-                        default(applycal)
-                        applycal(vis=msfilename, field=str(', '.join(mytargets)), spw = flagspw, gaintable=mygaintables,
-                                 gainfield=[str(', '.join(mypcals)),'',''],interp=['linear','','nearest'], calwt=[False], parang=False)
-                else:
-                        default(applycal)
-                        applycal(vis=msfilename, field=str(', '.join(mytargets)), spw = flagspw, gaintable=mygaintables,
-                                 gainfield=[str(', '.join(myampcals)),'',''],interp=['linear','','nearest'], calwt=[False], parang=False)                        
-        logging.info("Finished re-calibration.")
-        logging.info("A flagging summary is provided for the MS file.")
-        flagsummary(msfilename)
+	if target ==True:
+		if mypcals !=[]:
+			default(applycal)
+			applycal(vis=msfilename, field=str(', '.join(mytargets)), spw = flagspw, gaintable=mygaintables,
+				gainfield=[str(', '.join(mypcals)),'',''],interp=['linear','','nearest'], calwt=[False], parang=False)
+		else:
+			default(applycal)
+			applycal(vis=msfilename, field=str(', '.join(mytargets)), spw = flagspw, gaintable=mygaintables,
+				gainfield=[str(', '.join(myampcals)),'',''],interp=['linear','','nearest'], calwt=[False], parang=False)                        
+	plot_files = my_dig_plot(msfilename,myfields,myampcals,mypcals,mytargets,mycalsuffix,bptable,gntable)
+	logging.info("Finished re-calibration.")
+	logging.info("A flagging summary is provided for the MS file.")
+	flagsummary(msfilename)
 #############################################################
 # SPLIT step
 #############################################################
