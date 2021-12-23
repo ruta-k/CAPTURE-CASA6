d662177608854645d744d022cb212557cf9c222f
Alec Thomson <alecthomson0210@gmail.com>
2021-12-23 08:31:23 +0800

    Revert "Update vla_cals.list"
    This reverts commit 6df2b847e4a81392671c2766560113913093790b.

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    package_data={'': ['resources/*']},
+    package_data={'capture': ['gvfits','listscan','vla-cals.list']},
     license='MIT',
     classifiers=[
         # Trove classifiers
